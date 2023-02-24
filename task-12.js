322490db9cf63d0297e818812a8838f27284a91d
a805431 <gergana.ivanova@yara.com>
2023-02-24 16:02:40 +0200

    smol update

--- task-12.js
+++ task-12.js
@@ -1,13 +1,35 @@
+
+//function Spy() е custom constructor function
 function Spy(target, method) {
     // SOLUTION GOES HERE
+    this.count = 0;
+    
+    // constructor(){
+    //   this.count += 1;
+    //   target.apply(target, method);
+    // }
   }
 
   module.exports = Spy
 
-  var spy = Spy(console, 'error')
+  // var spy = Spy(console, 'error')
 
-  console.error('calling console.error')
-  console.error('calling console.error')
-  console.error('calling console.error')
+  // console.error('calling console.error')
+  // console.error('calling console.error')
+  // console.error('calling console.error')
 
-  console.log(spy.count) // 3
+  // console.log(spy.count) // 3
+
+  let mySpy = function(){
+    let count = 0;
+
+    return function(){
+      count++;
+      return count;
+    }
+  }();
+
+  console.log(mySpy());
+  console.log(mySpy());
+  console.log(mySpy());
+  console['log']('hello');
